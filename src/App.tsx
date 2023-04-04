e236fb8a28bb98193a0e8e0ff48792b2ec57bbe2
alumag <10659903+alumag@users.noreply.github.com>
2023-04-04 18:15:02 +0300

    CI: configure eslint
    Signed-off-by: alumag <10659903+alumag@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   const [count, setCount] = useState(0);
 
   return (
     <div className='App'>
       <div>
-        <a href='https://vitejs.dev' target='_blank'>
+        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
           <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href='https://reactjs.org' target='_blank'>
+        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
@@ -26,6 +26,6 @@ function App() {
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </div>
   );
-}
+};
 
 export default App;

0d8ab744e46fcc177b234607d22b06987b61ac42
alumag <10659903+alumag@users.noreply.github.com>
2023-04-04 18:31:03 +0300

    less: load with vite
    Signed-off-by: alumag <10659903+alumag@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,11 @@ import eslint from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), eslint()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
 });

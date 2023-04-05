cd537ce8d15f3dc9169d3aadc4c383145c8738ca
alumag <10659903+alumag@users.noreply.github.com>
2023-04-05 05:09:19 +0300

    For starts: display all data as descriptions
    Signed-off-by: alumag <10659903+alumag@users.noreply.github.com>

--- src/services/weather/Weather.tsx
+++ src/services/weather/Weather.tsx
@@ -1,6 +1,26 @@
+import { Card, Descriptions, Spin } from 'antd';
+import { useMemo } from 'react';
+import { useCurrentWeather } from './hooks';
+
 interface Props {
   location: string;
 }
 export const Weather: React.FC<Props> = ({ location }) => {
-  return <div></div>;
+  const { currentWeather, refetch } = useCurrentWeather(location);
+
+  const descriptions = useMemo(() => {
+    if (!currentWeather) {
+      return <Spin />;
+    }
+
+    return Object.entries(currentWeather).map(([key, value]) => (
+      <Descriptions.Item label={key}>{String(value)}</Descriptions.Item>
+    ));
+  }, [currentWeather]);
+
+  return (
+    <Card title='Weather'>
+      <Descriptions>{descriptions}</Descriptions>
+    </Card>
+  );
 };

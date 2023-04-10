926f482738b6f34eb91446f73da5f6e6ed91502e
alumag <10659903+alumag@users.noreply.github.com>
2023-04-10 19:16:21 +0300

    Weather Card: title
    Signed-off-by: alumag <10659903+alumag@users.noreply.github.com>

---
+++ src/services/weather/components/title/index.tsx
@@ -0,0 +1,30 @@
+import { Typography } from 'antd';
+import { useMemo } from 'react';
+import { WeatherApiCondition } from '../../redux';
+import style from './Title.module.css';
+interface Props {
+  condition?: WeatherApiCondition;
+}
+
+const Title: React.FC<Props> = ({ condition }) => {
+  const icon = useMemo(() => {
+    return condition ? (
+      <img className={style.icon} src={condition.icon} alt='weather-condition-icon' />
+    ) : null;
+  }, [condition]);
+
+  return (
+    <div className={style.titleContainer}>
+      <span>
+        <Typography className={style.bold}>
+          {icon}
+          Weather
+          {icon}
+        </Typography>
+      </span>
+      <Typography className={style.condition}>{condition?.text}</Typography>
+    </div>
+  );
+};
+
+export default Title;

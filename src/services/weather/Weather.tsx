73f0f3802416e4bb1fd45576e83fc075f03240c4
alumag <10659903+alumag@users.noreply.github.com>
2023-04-10 22:20:35 +0300

    Finished the Weather component
    Signed-off-by: alumag <10659903+alumag@users.noreply.github.com>

--- src/services/weather/Weather.tsx
+++ src/services/weather/Weather.tsx
@@ -1,5 +1,8 @@
-import { Card, Descriptions, Spin } from 'antd';
+import { ReloadOutlined } from '@ant-design/icons';
+import { Button, Card, Col, Empty, Row, Statistic, Typography } from 'antd';
 import { useMemo } from 'react';
+import Temperature from './components/temperature';
+import Title from './components/title';
 import { useCurrentWeather } from './hooks';
 
 interface Props {
@@ -8,19 +11,52 @@ interface Props {
 export const Weather: React.FC<Props> = ({ location }) => {
   const { currentWeather, refetch } = useCurrentWeather(location);
 
-  const descriptions = useMemo(() => {
+  const reload = useMemo(() => {
+    return (
+      <Button onClick={refetch} type='link'>
+        <ReloadOutlined />
+      </Button>
+    );
+  }, [refetch]);
+
+  const title = useMemo(() => {
     if (!currentWeather) {
-      return <Spin />;
+      return <Title />;
     }
 
-    return Object.entries(currentWeather).map(([key, value]) => (
-      <Descriptions.Item label={key}>{String(value)}</Descriptions.Item>
-    ));
+    return <Title condition={currentWeather.condition} />;
+  }, [currentWeather]);
+
+  const content = useMemo(() => {
+    if (!currentWeather) {
+      return <Empty />;
+    }
+
+    const { temp_c, temp_f, feelslike_c, feelslike_f, humidity, wind_mph } = currentWeather;
+    return (
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+        <Col>
+          <Temperature
+            temp_c={temp_c}
+            temp_f={temp_f}
+            feelslike_c={feelslike_c}
+            feelslike_f={feelslike_f}
+            unit='celsius'
+          />
+        </Col>
+        <Col>
+          <Statistic title='humidity' value={humidity} suffix='%' />
+        </Col>
+        <Col>
+          <Statistic title='wind speed' value={wind_mph} suffix={<Typography>mph</Typography>} />
+        </Col>
+      </Row>
+    );
   }, [currentWeather]);
 
   return (
-    <Card title='Weather'>
-      <Descriptions>{descriptions}</Descriptions>
+    <Card title={title} extra={reload}>
+      {content}
     </Card>
   );
 };
